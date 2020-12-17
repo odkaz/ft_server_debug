fb2b8e6beefaabd21c25e92d641812d36037375a
odkaz <one.destiny.japan@gmail.com>
2020-12-17 13:20:43 +0900

    wordpress doesnt show

--- srcs/init_docker.sh
+++ srcs/init_docker.sh
@@ -1,5 +1,4 @@
 # !/bin/sh
-service nginx start
 service mysql start
 mysql -e "CREATE DATABASE wpdb;"
 mysql -e "CREATE USER 'wpuser'@'localhost' identified by 'dbpassword';"
@@ -7,6 +6,8 @@ mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
 chown -R www-data:www-data /var/www/html/wordpress
 ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+service nginx start
 service php7.3-fpm start
 echo "init_docker"
 bash
